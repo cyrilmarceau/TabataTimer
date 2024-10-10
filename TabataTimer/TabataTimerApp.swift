d52a9f68e7945a783aa0086cd07f113d142f4f33
Cyril Marceau <cyril.marceau.dev@gmail.com>
2024-10-10 09:39:43 +0200

    Initial Commit

---
+++ TabataTimer/TabataTimerApp.swift
@@ -0,0 +1,17 @@
+//
+//  TabataTimerApp.swift
+//  TabataTimer
+//
+//  Created by MARCEAU Cyril on 10/10/2024.
+//
+
+import SwiftUI
+
+@main
+struct TabataTimerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ TabataTimerTests/TabataTimerTests.swift
@@ -0,0 +1,17 @@
+//
+//  TabataTimerTests.swift
+//  TabataTimerTests
+//
+//  Created by MARCEAU Cyril on 10/10/2024.
+//
+
+import Testing
+@testable import TabataTimer
+
+struct TabataTimerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
