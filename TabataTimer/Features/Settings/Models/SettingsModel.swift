67eca7b80f0e922c63ad73d8a316e034ab9660f0
Cyril Marceau <cyril.marceau.dev@gmail.com>
2024-10-10 11:49:41 +0200

    feat(settings): add SettingsModel with sound and time format options

--- TabataTimer/Features/Settings/Models/SettingsModel.swift
+++ TabataTimer/Features/Settings/Models/SettingsModel.swift
@@ -7,6 +7,16 @@
 
 import Foundation
 
-struct SettingsModel {
-    
+struct SettingsModel:  Codable, Identifiable {
+    var id: UUID = UUID()
+    var soundLevel: Int = 0
+    var enabledSound: Bool = false
+    var enabledVibration: Bool = false
+    var timeFormat: TimeFormat = .mmss
+}
+
+enum TimeFormat: String, Codable, CaseIterable {
+    case s = "s"
+    case mmss = "MMss"
+    case hhmmss = "HHMMSS"
 }
